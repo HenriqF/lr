9b2ee1d3a6ea49baf2ff1d23ababa1b0bbf56297
Henrique Reinaldi <carlosofalmeida@gmail.com>
2026-02-03 01:47:44 -0300

    Add files via upload

--- listrun.c
+++ listrun.c
@@ -3,8 +3,6 @@
 #include <sys/time.h>
 #include "readwrite.h"
 
-#define F_TIME 0x01
-
 int startsWith(char* a, char* b){
     size_t la = strlen(a);
     size_t lb = strlen(b);
@@ -17,14 +15,13 @@ int startsWith(char* a, char* b){
 void runLr(DWORD wait){
     char ppath[MAX_PATH];
     GetCurrentDirectoryA(MAX_PATH, ppath);
-
-    char ls_path[MAX_PATH+3];
-    snprintf(ls_path, MAX_PATH+3, "%s\\lr", ppath);
+    char lr_path[MAX_PATH+3];
+    snprintf(lr_path, MAX_PATH+3, "%s\\lr", ppath);
 
     size_t size;
     char* content;
 
-    FILE* f = fopen(ls_path, "rb");
+    FILE* f = fopen(lr_path, "rb");
     if (!f){
         printf("sem arquivo lr");
         return;
@@ -33,29 +30,35 @@ void runLr(DWORD wait){
     readFile(f, &size, &content);
     fclose(f);
 
-    char command[512];
+    char command[1024];
     int ll = 0;
-
 
     for(int i = 0; i <= size; i++){
         if(content[i] == '\n' || i == size){
             snprintf(command, (size_t)i-ll+1, "%s", content+ll);
-            system(command);
+
+            if (startsWith(command, "//") == 1 || startsWith(command, "lr")){}
+            else if (startsWith(command, "#") == 1){
+                printf("Aperte enter para continuar...");
+                int c;
+                while ((c = getchar()) != '\n' && c != EOF) {}
+            }
+            else{
+                system(command);
+                if (i != size) Sleep(wait);
+            }
+
             ll = i+1;
-            if (i != size) Sleep(wait);
         }
     }
 }
 
 int main(int argc, char** argv){
-
-    int flags = 0;
     DWORD wait_time = 0;
 
     if (argc != 1){
         for (int i = 1 ; i < argc; i++){
             if (startsWith(argv[i], "-t")){
-                flags |= F_TIME;
                 char time_str[10];
                 snprintf(time_str, 10, "%s", argv[i]+2);
                 wait_time = (DWORD)atoi(time_str);
@@ -64,6 +67,5 @@ int main(int argc, char** argv){
     }
     
     runLr(wait_time);
-
     return 0;
 }
